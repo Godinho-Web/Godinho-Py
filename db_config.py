8abe83d97fa0c9d7401ba5f3abb7f6d4626b0963
Luiz Felipe Godinho <67346095+Godinho-Web@users.noreply.github.com>
2022-06-16 12:01:08 -0300

    Godinho Py

---
+++ db_config.py
@@ -0,0 +1,10 @@
+from server import app
+from flaskext.mysql import MySQL
+mysql = MySQL()
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'masterkey'
+app.config['MYSQL_DATABASE_DB'] = 'bdjogos'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
